886a96384849118e86776b2908d69f096ea2bf46
JOAQUIN DE FERRARI GONZALEZ <joaquin.deferrari@sansano.usm.cl>
2022-06-28 12:24:40 -0400

    20k iteraciones

--- Main.cpp
+++ Main.cpp
@@ -45,7 +45,7 @@ int main() {
             //time(&start);
             auto t1 = high_resolution_clock::now();
             
-            ListaVehiculos vehiculos = generarSoluciones(10000,inst,clientes,estaciones,depot);
+            ListaVehiculos vehiculos = generarSoluciones(20000,inst,clientes,estaciones,depot);
 
             auto t2 = high_resolution_clock::now();
             auto ms_int = duration_cast<milliseconds>(t2 - t1);
